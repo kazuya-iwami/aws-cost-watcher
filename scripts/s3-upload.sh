80114ea658d8d1d3771c9fabf1aabcc12cb68252
Iwami, Kazuya <kiiwami@amazon.co.jp>
2018-11-09 18:16:18 +0900

    Update script

--- scripts/s3-upload.sh
+++ scripts/s3-upload.sh
@@ -6,5 +6,5 @@ pip install -r ../requirements.txt -t ../function
 (cd ../function; zip -r9 ../function.zip .)
 
 # Upload files
-aws s3 cp ../function.zip s3://tokyo.k.iwami/cost-watcher/
-aws s3 cp ../template.yaml s3://tokyo.k.iwami/cost-watcher/
+aws s3 cp ../function.zip s3://tokyo.k.iwami/cost-watcher/ --acl authenticated-read
+aws s3 cp ../template.yaml s3://tokyo.k.iwami/cost-watcher/ --acl authenticated-read
